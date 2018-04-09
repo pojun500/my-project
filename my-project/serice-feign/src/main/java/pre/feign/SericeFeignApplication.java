826e48b905e845e1dfd1c114cf5e2eb5631ca6c4
许晓军 <@DESKTOP-PV10RIV>
2018-04-09 20:34:59 +0800

    1

--- my-project/serice-feign/src/main/java/pre/feign/SericeFeignApplication.java
+++ my-project/serice-feign/src/main/java/pre/feign/SericeFeignApplication.java
@@ -2,8 +2,12 @@ package pre.feign;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SericeFeignApplication {
 
 	public static void main(String[] args) {
