ab46d4d86abb0212d01746bb47fee7b6755344d2
许晓军 <@DESKTOP-PV10RIV>
2018-04-09 20:17:24 +0800

    1

---
+++ my-project/serice-feign/src/main/java/pre/feign/SericeFeignApplication.java
@@ -0,0 +1,12 @@
+package pre.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SericeFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SericeFeignApplication.class, args);
+	}
+}
